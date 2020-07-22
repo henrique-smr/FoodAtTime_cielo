19105ec9f991b67803235a0cc9f084959e3fa72b
Henrique <henriquesouto@id.uff.br>
2020-07-21 21:05:14 -0300

    Exemplo de transação adicionada

--- index.js
+++ index.js
@@ -43,91 +43,48 @@ const sale = new Sale(customer, bill, merchant)
 
 cieloPostSale(cieloOperation.BANKSLIP.CREATE, sale)
 
-/*
-
-const cieloMerchantInfo = {
-	merchantId: myMerchant.merchantId,
-	merchantKey: myMerchant.merchantKey,
-	requestId: '0000001', // Opcional - Identificação do Servidor na Cielo
-	sandbox: true, // Opcional - Ambiente de Testes
-	debug: true // Opcional - Exibe os dados enviados na requisição para a Cielo
-};
-
-const cieloHolder = new Cielo.Cielo(cieloMerchantInfo);
-
-const boletoParams = {
-		merchantOrderId: '20180531222333',
-		customer: {
-			name: 'Comprádor Boleto Cíéló Áá',
-			identity: '1234567890',
-			address: {
-				street: 'Avenida Marechal Câmara',
-				number: '160',
-				complement: 'Sala 934',
-				zipCode: '22750012',
-				district: 'Centro',
-				city: 'Rio de Janeiro',
-				state: 'RJ',
-				country: 'BRA'
-			}
-		},
-		payment: {
-			type: 'Boleto',
-			installments:1,
-			authenticate:false,
-			amount: 15700,
-			provider: 'Bradesco2',
-			address: 'Rua Teste',
-			boletoNumber: '123',
-			assignor: 'Empresa Teste',
-			demonstrative: 'Desmonstrative Teste',
-			expirationDate: '2020-05-01',
-			identification: '11884926754',
-			instructions: 'Aceitar somente até a data de vencimento, após essa data juros de 1% dia.'
-		}
-	}
-
-function onBoletoCreate(data)
+/* RESPOSTA:
 {
-	console.log(data)
-}
-function onConsulta(consulta)
-{
-	console.log(consulta)
+  merchantOrderId: '202072119',
+  customer: {
+    name: 'Jose da Silva',
+    identity: '1234567890',
+    identityType: 'CPF',
+    birthdate: '1995-08-31',
+    address: {
+      street: 'Avenida Marechal Camara',
+      number: '160',
+      complement: 'Sala 934',
+      zipCode: '22750012',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      country: 'BRA',
+      district: 'Centro'
+    }
+  },
+  payment: {
+    instructions: 'Aceitar somente ate a data de vencimento, apos essa data juros de 1% dia.',
+    expirationDate: '2020-05-01',
+    demonstrative: 'Desmonstrative Teste',
+    url: 'https://transactionsandbox.pagador.com.br/post/pagador/reenvia.asp/2d33a113-0c4d-49a2-a13e-4c374dbb5c05',
+    boletoNumber: '123-2',
+    barCodeNumber: '00095824200000157009999250000000012399999990',
+    digitableLine: '00099.99921 50000.000013 23999.999909 5 82420000015700',
+    assignor: 'Empresa Teste',
+    address: 'Rua Teste',
+    identification: '11884926754',
+    bank: 0,
+    amount: 15700,
+    receivedDate: '2020-07-21 19:51:47',
+    provider: 'Simulado',
+    status: 1,
+    isSplitted: false,
+    paymentId: '2d33a113-0c4d-49a2-a13e-4c374dbb5c05',
+    type: 'Boleto',
+    currency: 'BRL',
+    country: 'BRA',
+    links: [ [Object] ]
+  }
 }
 
-cieloHolder.bankSlip.create(boletoParams)
-	.then((data) => onBoletoCreate(data))
-	.catch((error) => {
-		console.log("ERROR ",error)
-	})
-
-cieloHolder.consult.merchantOrderId({"merchantOrderId":boletoParams.merchantOrderId})
-	.then((consulta) => onConsulta(consulta))
-	.catch((error) => {
-		console.log("ERROR ",error)
-	})
-
-const sellParams = {
-	customer: {
-		name: "Comprador crédito",
-	},
-	merchantOrderId: "2014111703",
-	payment: {
-		amount: 10000, // R$100,00
-		creditCard: {
-			brand: EnumBrands.VISA,
-			cardNumber: "4532117080573701",
-			holder: "Comprador T Cielo",
-			expirationDate: "12/2021",
-		},
-		installments: 1,
-		softDescriptor: "FAT",
-		type: EnumCardType.CREDIT,
-		capture: false,
-	},
-};
 */
-
-
-//foodattime-1595290302345
